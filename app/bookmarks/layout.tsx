1765eae13bce8bd565464bee88571afacae1c38f
Lethukuthula Ndwandwe <150596482+ELjayMedia@users.noreply.github.com>
2025-07-30 16:44:39 +0200

    refactor: add ProtectedLayout component

--- app/bookmarks/layout.tsx
+++ app/bookmarks/layout.tsx
@@ -1,6 +1,1 @@
-import type React from "react"
-import { ProtectedRoute } from "@/components/ProtectedRoute"
-
-export default function BookmarksLayout({ children }: { children: React.ReactNode }) {
-  return <ProtectedRoute>{children}</ProtectedRoute>
-}
+export { default } from "@/components/ProtectedLayout"
