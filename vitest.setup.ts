810ce1e6a506b7a86ee6dcd8d5fbf180fc61d911
Lethukuthula Ndwandwe <150596482+ELjayMedia@users.noreply.github.com>
2025-09-10 13:42:06 +0200

    Add testing setup and coverage

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,6 @@
 import { expect } from 'vitest'
-import matchers from '@testing-library/jest-dom/matchers'
-expect.extend(matchers)
+import * as matchers from '@testing-library/jest-dom/matchers'
+expect.extend(matchers as any)
 class ResizeObserver {
   observe() {}
   unobserve() {}
